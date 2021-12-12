82930b3aef5fb0a95d2c54b39601057ab1827a88
Federico Bertani <fedbertani@gmail.com>
2021-12-12 15:40:38 +0100

    improved download_dataset script with check if file/folders exists

--- download_dataset.py
+++ download_dataset.py
@@ -5,18 +5,23 @@ import urllib.request
 import requests
 from tqdm.autonotebook import tqdm  # tqdm/tqdm: A Fast, Extensible Progress Bar
 
-os.makedirs("data", exist_ok=True)
-# download and parse json containing image URLs
-urllib.request.urlretrieve(
-    "https://raw.githubusercontent.com/zalandoresearch/feidegger/master/data/FEIDEGGER_release_1.1.json")
-data = None
-with open("FEIDEGGER_release_1.1.json") as f:
-    data = json.load(f)
+feidegger_file_name = "FEIDEGGER_release_1.1.json"
+feidegger_url = "https://raw.githubusercontent.com/zalandoresearch/feidegger/master/data/FEIDEGGER_release_1.1.json"
+data_folder = "data/"
 
-# use requests library Session object to take advantage of HTTP persistent connection
-# by reusing TCP connection
-s = requests.Session()
-for i, element in enumerate(tqdm(data)):
-    image = s.get(element['url'])
-    with open("data/{}.jpg".format(i), 'wb') as f:
-        f.write(image.content)
+if not os.path.exists(feidegger_file_name):
+    # download and parse json containing image URLs
+    urllib.request.urlretrieve(feidegger_url, feidegger_file_name)
+
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+    data = None
+    with open(feidegger_file_name) as f:
+        data = json.load(f)
+    # use requests library Session object to take advantage of HTTP persistent connection
+    # by reusing TCP connection
+    s = requests.Session()
+    for i, element in enumerate(tqdm(data)):
+        image = s.get(element['url'])
+        with open(data_folder + "{}.jpg".format(i), 'wb') as f:
+            f.write(image.content)
