27f9b17914aa5cf22ca3543f507d448781818376
Federico Bertani <fedbertani@gmail.com>
2021-12-14 01:34:30 +0100

    cleanup

--- generation/dcgan_train.py
+++ generation/dcgan_train.py
@@ -1,16 +1,13 @@
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.optim as optim
-import torchvision.utils as vutils
-from IPython.display import HTML
 from torch import nn
 from torch.utils.data import DataLoader
 
 from dcgan_discriminator import FashionDiscriminator
-from dcgan_generator import FashionGeneator
 from load_dataset import load_dataset
+from reconstruction.models.conv_autoencoder import ConvAutoEncoder
 
 # Size of z latent vector (i.e. size of generator input)
 latent_space_dim = 128
@@ -43,15 +40,15 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
     device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
     # Create the generator
-    generator = FashionGeneator(ngpu).to(device)
+    generator = ConvAutoEncoder().to(device)
 
     # Handle multi-gpu if desired
-    if (device.type == 'cuda') and (ngpu > 1):
-        generator = nn.DataParallel(generator, list(range(ngpu)))
+    # if (device.type == 'cuda') and (ngpu > 1):
+    #    generator = nn.DataParallel(generator, list(range(ngpu)))
 
     # Apply the weights_init function to randomly initialize all weights
     #  to mean=0, stdev=0.02.
-    generator.apply(weights_init)
+    # generator.apply(weights_init)
 
     # Create the Discriminator
     discriminator = FashionDiscriminator(ngpu).to(device)
@@ -66,6 +63,7 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
 
     # Initialize BCELoss function
     criterion = nn.BCELoss()
+    criterion2 = nn.MSELoss()
 
     # Create batch of latent vectors that we will use to visualize
     #  the progression of the generator
@@ -77,12 +75,14 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
 
     # Setup Adam optimizers for both G and D
     optimizerD = optim.Adam(discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
-    optimizerG = optim.Adam(generator.parameters(), lr=lr, betas=(beta1, 0.999))
+    # optimizerD = optim.Adam(discriminator.parameters())
+    # optimizerG = optim.Adam(generator.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizerG = optim.Adam(generator.parameters(), lr=0.1)
 
     # Training Loop
 
     # Lists to keep track of progress
-    img_list = []
+    # img_list = []
     generator_losses = []
     discriminator_losses = []
     iters = 0
@@ -104,26 +104,26 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
             b_size = real_cpu.size(0)
             label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
             # Forward pass real batch through D
-            output = discriminator(real_cpu).view(-1)
+            output_d = discriminator(real_cpu).view(-1)
             # Calculate loss on all-real batch
-            errD_real = criterion(output, label)
+            errD_real = criterion(output_d, label)
             # Calculate gradients for D in backward pass
             errD_real.backward()
-            D_x = output.mean().item()
+            D_x = output_d.mean().item()
 
             ## Train with all-fake batch
             # Generate batch of latent vectors
-            noise = torch.randn(b_size, latent_space_dim, 1, 1, device=device)
+            # noise = torch.randn(b_size, latent_space_dim, 1, 1, device=device)
             # Generate fake image batch with G
-            fake = generator(noise)
+            _, fake = generator(data)
             label.fill_(fake_label)
             # Classify all fake batch with D
-            output = discriminator(fake.detach()).view(-1)
+            output_d = discriminator(fake.detach()).view(-1)
             # Calculate D's loss on the all-fake batch
-            errD_fake = criterion(output, label)
+            errD_fake = criterion(output_d, label)
             # Calculate the gradients for this batch, accumulated (summed) with previous gradients
             errD_fake.backward()
-            D_G_z1 = output.mean().item()
+            D_G_z1 = output_d.mean().item()
             # Compute error of D as sum over the fake and the real batches
             errD = errD_real + errD_fake
             # Update D
@@ -135,12 +135,13 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
             generator.zero_grad()
             label.fill_(real_label)  # fake labels are real for generator cost
             # Since we just updated D, perform another forward pass of all-fake batch through D
-            output = discriminator(fake).view(-1)
+            output_d = discriminator(fake).view(-1)
             # Calculate G's loss based on this output
-            errG = criterion(output, label)
+            # errG = 0.001* criterion(output_d, label) + 0.999*criterion2(fake,data)
+            errG = criterion2(fake, data)
             # Calculate gradients for G
             errG.backward()
-            D_G_z2 = output.mean().item()
+            D_G_z2 = output_d.mean().item()
             # Update G
             optimizerG.step()
 
@@ -154,12 +155,6 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
             generator_losses.append(errG.item())
             discriminator_losses.append(errD.item())
 
-            # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % 500 == 0) or ((epoch == epochs - 1) and (i == len(dataloader) - 1)):
-                with torch.no_grad():
-                    fake = generator(fixed_noise).detach().cpu()
-                    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-
             iters += 1
 
     plt.figure(figsize=(10, 5))
@@ -169,15 +164,6 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
     plt.xlabel("iterations")
     plt.ylabel("Loss")
     plt.legend()
-    plt.show()
-
-    # %%capture
-    fig = plt.figure(figsize=(8, 8))
-    plt.axis("off")
-    ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
-    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-
-    HTML(ani.to_jshtml())
 
     # Grab a batch of real images from the dataloader
     real_batch = next(iter(dataloader))
@@ -187,16 +173,21 @@ def dcgan_train(dataset, epochs=15, batch_size=5):
     plt.subplot(1, 2, 1)
     plt.axis("off")
     plt.title("Real Images")
-    plt.imshow(
-        np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(), (1, 2, 0)))
+    img = np.transpose(feidegger[0], (1, 2, 0)).detach().numpy()
+    img = (img + 1) / 2
+    img = (img * 255).astype(np.uint8)
+    plt.imshow(img)
 
     # Plot the fake images from the last epoch
     plt.subplot(1, 2, 2)
     plt.axis("off")
     plt.title("Fake Images")
-    plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
+    img = generator(feidegger[0][None, :])[1].detach().numpy().reshape(3, 128, 128)
+    img = (img + img.min()) / (img.max() - img.min())
+    img = (img * 255).astype(np.uint8)
+    plt.imshow(np.transpose(img, (1, 2, 0)))
     plt.show()
 
 
-feidegger = load_dataset(0.1)
-dcgan_train(feidegger, epochs=200, batch_size=128)
+feidegger = load_dataset(0.05)
+dcgan_train(feidegger, epochs=100, batch_size=5)
--- reconstruction/README.md
+++
@@ -1,5 +0,0 @@
-Task: with encoder-decoder networks, measure their capacity of finding a suitable encoding able to generate back the
-original image as close as possible.
-
-The encoding has to have 256 dimensions, like Fashion DNA.
-
